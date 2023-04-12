ff3584a5fffffa99463917b11b7e5650ffb6131e
jiwan <audwldhks0@gmail.com>
2023-04-12 16:22:29 +0900

    출력

---
+++ ch00/sec00/test01.java
@@ -0,0 +1,17 @@
+package ch00.sec00;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, JAVA");
+		System.out.println("한글자바");
+		System.out.println("Hello, JAVA");
+		System.out.println("Hello, JAVA");
+		System.out.println("Hello, JAVA");
+	
+
+	
+	
+	}
+}
