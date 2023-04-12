3f7529941dbb9436c668faa87b3c593d3992a01e
jiwan <audwldhks0@gmail.com>
2023-04-12 16:48:48 +0900

    더하기

---
+++ ch00/sec01/test02.java
@@ -0,0 +1,15 @@
+package ch00.sec01;
+
+public class test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		int y = 3;
+		
+		System.out.println("i + Y = "+ (i+y));
+
+	
+	
+	}
+}
